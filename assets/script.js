0bcb7b045e52f383da41593a3cf967849a2dd467
AdilTehranli <adiljt@code.edu.az>
2023-06-03 15:12:24 +0400

    Crated

---
+++ assets/script.js
@@ -0,0 +1,50 @@
+//#region Yasin 
+
+
+// let ver=prompt('yasinizi daxil edin');
+// if(ver>20 ){
+//     alert('yasiniz 20 den boyukdur')
+// }
+// else if(ver<20){
+//     alert('yasiniz 20 den kicikdir')
+// }
+// else{
+//     alert('yasiniz dogru deyil')
+// }
+//#endregion
+
+//#region  Tek ve cut ededin tapilmasi
+// function Agevalidaton(){
+//     let age=document.getElementById('num').value;
+
+//         if(age % 2==0){
+//             alert('cut eded')
+//         }else{
+//             alert('tek eded')
+//         }
+//     }
+//#endregion
+
+//#region dovr operatorlari
+    // for(let i=0;i<5;i++){
+    //     console.log(i);
+    // }
+
+
+    // for(let i=0;i<=5;i++){
+    //     console.log(i)
+    // }
+
+
+    // let x=0;
+    // while(x<20){
+    //     x++;
+    //     console.log(x);
+    // }
+
+    // let x=10;
+    // while(x>0){
+    //     console.log(x);
+    //     x--;    
+    // }
+//#endregion
